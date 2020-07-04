221a32a24f167f40f94b3c934d9030578d79b63b
Kuljeet Singh <singhkuljeet961988@gmail.com>
2020-07-04 14:23:05 +0530

    removed null items from list and implemented list cache-done

--- app/src/main/java/com/infosystest/databinding/BindingAdapters.kt
+++ app/src/main/java/com/infosystest/databinding/BindingAdapters.kt
@@ -2,7 +2,6 @@ package com.infosystest.databinding
 
 import android.text.TextUtils
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
 
@@ -10,21 +9,16 @@ class BindingAdapters {
     companion object {
         @BindingAdapter("newsAvatarUrl")
         @JvmStatic
-        fun setMovieAvatarUrl(imageView: ImageView, imageAvatarUrl: String) {
+        fun setNewsAvatarUrl(imageView: ImageView, newsAvatarUrl: String?) {
             val context = imageView.context
-            if (!TextUtils.isEmpty(imageAvatarUrl)) {
+            if (!TextUtils.isEmpty(newsAvatarUrl)) {
                 Picasso.with(context)
-                    .load(imageAvatarUrl)
+                    .load(newsAvatarUrl)
                     .centerCrop()
                     .fit()
                     .into(imageView)
             }
         }
 
-        @BindingAdapter("descriptionText")
-        @JvmStatic
-        fun setIntegerText(textView: TextView, descriptionText: String) {
-            textView.setText(descriptionText)
-        }
     }
 }
--- app/src/main/java/com/infosystest/ui/NewsListPresenter.kt
+++ app/src/main/java/com/infosystest/ui/NewsListPresenter.kt
@@ -5,6 +5,7 @@ import androidx.annotation.VisibleForTesting
 import com.infosystest.network.repository.Repository
 import com.infosystest.network.schedulers.SchedulerProvider
 import io.reactivex.disposables.Disposable
+import java.util.*
 
 class NewsListPresenter(
     val newsListViewModel: NewsListViewModel,
@@ -17,12 +18,26 @@ class NewsListPresenter(
         loadShimmerView()
         disposable = repository.getNewsList(
         )?.subscribeOn(getSchedulerProvider().io())
+            ?.map(this::mapNewsResponseToListItems)
             ?.observeOn(getSchedulerProvider().ui())
             ?.subscribe(
                 { newsListResponse -> onRepoListSuccess(newsListResponse) },
                 { _ -> onNetworkFailure() })
     }
-
+    private fun mapNewsResponseToListItems(newsListResponse: NewsListResponse): List<NewsListResponse.RowsEntity>? {
+        val newsItems: List<NewsListResponse.RowsEntity> = ArrayList()
+        if(newsListResponse.rows!!.isNotEmpty()){
+            var listSize = newsListResponse.rows!!.size
+            for (index in 0 until listSize) {
+                var rowsEntity = newsListResponse.rows!![index]
+                if(rowsEntity.title.isNullOrBlank()&&rowsEntity.description.isNullOrBlank()){
+                    continue;
+                }
+                newsItems.toMutableList().add(rowsEntity)
+            }
+        }
+        return newsItems
+    }
     private fun loadShimmerView() {
         newsListViewModel.shimmerViewVisibility.value = View.VISIBLE
         newsListViewModel.newsListVisibility.value = View.GONE
@@ -42,10 +57,10 @@ class NewsListPresenter(
     }
 
     @VisibleForTesting
-    internal fun onRepoListSuccess(newsListResponse: NewsListResponse) {
+    internal fun onRepoListSuccess(newsListResponse: List<NewsListResponse.RowsEntity>?) {
         when {
-            (newsListResponse.rows!!.isNotEmpty()) -> {
-                newsListViewModel.newsListData.postValue(newsListResponse.rows)
+            (newsListResponse!!.isNotEmpty()) -> {
+                newsListViewModel.newsListData.postValue(newsListResponse)
                 loadListView()
             }
             else -> loadRetryView()
